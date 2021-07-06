1d261ede7d3b2d1c33ca577be9626f3ae0ad0b8b
hanzhijie <hanzhijie@58.com>
2021-07-06 11:16:12 +0800

    update all

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	private final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		logger.info("日志日志日志");
+		return String.format("Hello %s!", name);
+	}
+
+
+}
